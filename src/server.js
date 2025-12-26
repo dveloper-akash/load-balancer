99728ed0770254a59537fddaabdd96d6c2477534
Akash <developerakash99@gmail.com>
2025-12-26 10:02:41 +0530

    feat: add lb server

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { startHealthChecks } from "./health";
+import { workers } from "./worker";
+import { proxyRequest } from "./proxy";
+const app=express();
+
+startHealthChecks(workers);
+
+app.get("/metrics",metricsController);
+
+app.use(proxyRequest);
+
+const PORT=80;
+
+app.listen(PORT,()=>{
+    console.log(`Load Balancer running on port ${PORT}`);
+})
